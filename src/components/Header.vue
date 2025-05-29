6f87ed472fea808412589b159deb2761b66e0ceb
Muhammad Fajrul Khaq <fajrulm05@gmail.com>
2025-05-30 01:28:38 +0700

    feat: add gsap header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,78 @@
 <script setup>
 import gsap from 'gsap'
-import { onMounted } from 'vue'
-import { ref } from 'vue'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { onMounted, ref } from 'vue'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const hero1 = ref(null)
 const hero2 = ref(null)
-const header = ref(null)
 const headerTitle = ref(null)
 const headerSubtitle = ref(null)
 const headerSocials = ref(null)
 
 onMounted(() => {
-  gsap.set(hero1.value, { y: -100, opacity: 0 })
-  gsap.set(hero2.value, { y: -100, opacity: 0 })
+  gsap.set([hero1.value, hero2.value], { y: -200, opacity: 0 })
 
-  const tl = gsap.timeline({
-    repeat: -1,
+  const tl = gsap.timeline({ repeat: -1 })
+
+  tl.to(hero1.value, { y: 0, opacity: 1, duration: 0.3 }).to(hero1.value, {
+    y: 100,
+    opacity: 0,
+    duration: 0.3,
+    delay: 1,
   })
-  tl.to(hero1.value, {
+
+  tl.to(hero2.value, { y: 0, opacity: 1, duration: 0.3 }).to(hero2.value, {
+    y: 100,
+    opacity: 0,
+    delay: 1,
+    duration: 0.3,
+  })
+
+  gsap.set(headerTitle.value, { x: 200, opacity: 0 })
+  gsap.set(headerSubtitle.value, { y: 100, opacity: 0 })
+  gsap.set(headerSocials.value, { y: 100, opacity: 0 })
+
+  gsap.to(headerTitle.value, {
+    scrollTrigger: {
+      trigger: headerTitle.value,
+      start: 'top 80%',
+      end: 'bottom 20%',
+      toggleActions: 'play reverse play reverse',
+    },
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    ease: 'power3.out',
+  })
+
+  gsap.to(headerSubtitle.value, {
+    scrollTrigger: {
+      trigger: headerSubtitle.value,
+      start: 'top 90%',
+      end: 'bottom 10%',
+      toggleActions: 'play reverse play reverse',
+    },
     y: 0,
     opacity: 1,
-    duration: 0.3,
-  }).to(hero1.value, {
-    y: 100,
-    opacity: 0,
-    duration: 0.3,
-    delay: 1,
+    duration: 1,
+    ease: 'power3.out',
+    delay: 0.3,
   })
-  tl.to(hero2.value, {
+
+  gsap.to(headerSocials.value, {
+    scrollTrigger: {
+      trigger: headerSocials.value,
+      start: 'top 90%',
+      end: 'bottom 10%',
+      toggleActions: 'play reverse play reverse',
+    },
     y: 0,
-    opacity: 4,
-    duration: 0.3,
-  }).to(hero2.value, {
-    y: 100,
-    opacity: 0,
-    delay: 1,
-    duration: 0.3,
+    opacity: 1,
+    duration: 1,
+    ease: 'power3.out',
+    delay: 0.3,
   })
 })
 </script>
@@ -54,19 +91,32 @@ onMounted(() => {
       </div>
       <div class="header-content">
         <div class="hero-container">
-          <img ref="hero1" src="../assets/images/hero.svg" alt="hero" class="hero hero1" />
-          <img ref="hero2" src="../assets/images/hero2.svg" alt="hero" class="hero hero2" />
+          <img
+            ref="hero1"
+            src="../assets/images/hero.svg"
+            alt="hero"
+            class="hero hero1"
+            loading="lazy"
+          />
+          <img
+            ref="hero2"
+            src="../assets/images/hero2.svg"
+            alt="hero"
+            class="hero hero2"
+            loading="lazy"
+          />
         </div>
-        <h1 class="header-title">
+        <h1 class="header-title" ref="headerTitle">
           Innovative <br />
           Solutions <br />
           for <br />
           Animals
         </h1>
       </div>
-      <div class="header-subtitle">charity organization</div>
+      <div class="header-subtitle" ref="headerSubtitle">charity organization</div>
     </div>
-    <div class="header-socials">
+
+    <div class="header-socials" ref="headerSocials">
       <img src="../assets/images/yt.svg" alt="icon" class="social-icon" />
       <img src="../assets/images/ig.svg" alt="icon" class="social-icon" />
       <img src="../assets/images/fb.svg" alt="icon" class="social-icon" />
@@ -75,5 +125,3 @@ onMounted(() => {
     </div>
   </header>
 </template>
-
-<style scoped></style>
