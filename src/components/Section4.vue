5a60494754de463d89ec520755fb87f9e5a6059f
Muhammad Fajrul Khaq <fajrulm05@gmail.com>
2025-05-29 16:33:41 +0700

    feat: add section4

---
+++ src/components/Section4.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { helpTitles } from '../data/helpTitles'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { onMounted } from 'vue'
+
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {})
+</script>
+
+<template>
+  <section class="section4">
+    <div class="container4">
+      <h2>We want to help</h2>
+      <div>
+        homeless packs, volunteers, guardians, overstayers, shelters and other organisations
+      </div>
+      <div class="help-wrap">
+        <div class="help-bg" v-for="(title, index) in helpTitles" :key="index">
+          <div class="help-item">
+            <p class="help-title">{{ title }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,11 +1,13 @@
 <script setup>
-import Header from '../components/Header.vue'
+import HeaderAboutUs from '../components/Header.vue'
 import Section2 from '../components/Section2.vue'
-import Section3 from '@/components/section3.vue'
+import Section3 from '@/components/Section3.vue'
+import Section4 from '@/components/Section4.vue'
 </script>
 
 <template>
-  <Header />
+  <HeaderAboutUs />
   <Section2 />
   <Section3 />
+  <Section4 />
 </template>
