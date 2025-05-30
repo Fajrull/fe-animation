8b518f320d3b7450b5d66c98cd895ae77e6bc8a6
Muhammad Fajrul Khaq <fajrulm05@gmail.com>
2025-05-30 11:27:33 +0700

    fix: url img

--- src/data/supportUs.js
+++ src/data/supportUs.js
@@ -1,72 +1,87 @@
+import support1 from '../assets/images/support1.jpg'
+import support2 from '../assets/images/support2.jpg'
+import support3 from '../assets/images/support3.jpg'
+import support4 from '../assets/images/support4.jpg'
+import support5 from '../assets/images/support5.jpg'
+import support6 from '../assets/images/support6.jpg'
+import support7 from '../assets/images/support7.jpg'
+import support8 from '../assets/images/support8.jpg'
+import support9 from '../assets/images/support9.jpg'
+import support10 from '../assets/images/support10.jpg'
+import support11 from '../assets/images/support11.jpg'
+import support12 from '../assets/images/support12.jpg'
+import support13 from '../assets/images/support13.jpg'
+import support14 from '../assets/images/support14.jpg'
+
 export const supportUs = [
   {
     name: 'Oleksiy Tritenko',
     title: 'theater and film actor',
-    img: '../../src/assets/images/support1.jpg',
+    img: support1,
   },
   {
     name: 'Stanislav Boklan',
     title: "People's Artist of Ukraine",
-    img: '../../src/assets/images/support2.jpg',
+    img: support2,
   },
   {
     name: 'Alexander Rudinsky',
     title: 'theater and film actor',
-    img: '../../src/assets/images/support3.jpg',
+    img: support3,
   },
   {
     name: 'Oksana Zhdanova',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support4.jpg',
+    img: support4,
   },
   {
     name: 'Natalka Denisenko',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support5.jpg',
+    img: support5,
   },
   {
     name: 'Maksym Samchyk',
     title: 'theater and film actor',
-    img: '../../src/assets/images/support6.jpg',
+    img: support6,
   },
   {
     name: 'Kateryna Vyshneva',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support7.jpg',
+    img: support7,
   },
   {
     name: 'Irma Vitovska',
     title: 'Honored Actress of Ukraine',
-    img: '../../src/assets/images/support8.jpg',
+    img: support8,
   },
   {
     name: 'Igor Portyanko',
     title: 'actor of theater and cinema',
-    img: '../../src/assets/images/support9.jpg',
+    img: support9,
   },
   {
     name: 'Dmytro Surzhikov',
     title: 'Honored Artist of Ukraine',
-    img: '../../src/assets/images/support10.jpg',
+    img: support10,
   },
   {
     name: 'Daria Barihashvili',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support11.jpg',
+    img: support11,
   },
   {
     name: 'Vera Kobzar',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support12.jpg',
+    img: support12,
   },
   {
     name: 'Andriy Fedynchyk',
     title: 'theater and film actor',
-    img: '../../src/assets/images/support13.jpg',
+    img: support13,
   },
   {
     name: 'Anastasia Nesterenko',
     title: 'theater and film actress',
-    img: '../../src/assets/images/support14.jpg',
+    img: support14,
   },
 ]
--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -1,61 +1,74 @@
+import team1 from '../assets/images/team1.jpg'
+import team2 from '../assets/images/team2.jpg'
+import team3 from '../assets/images/team3.jpg'
+import team4 from '../assets/images/team4.jpg'
+import team5 from '../assets/images/team5.jpg'
+import team6 from '../assets/images/team6.jpg'
+import team7 from '../assets/images/team7.jpg'
+import team8 from '../assets/images/team8.png'
+import team9 from '../assets/images/team9.jpg'
+import team10 from '../assets/images/team10.jpg'
+import hero from '../assets/images/hero.svg'
+import hero2 from '../assets/images/hero2.svg'
+
 export const teamMembers = [
   {
-    img: '../../src/assets/images/team1.jpg',
+    img: team1,
     name: 'Daryna Deriy',
     title: 'Chairman of the Board',
   },
   {
-    img: '../../src/assets/images/team2.jpg',
+    img: team2,
     name: 'Polina Snisarenko-Kulchytska',
     title: 'Curator of the Foundation',
   },
   {
-    img: '../../src/assets/images/team3.jpg',
+    img: team3,
     name: 'Bohdan Kulchytsky',
     title: 'Executive Manager',
   },
   {
-    img: '../../src/assets/images/team4.jpg',
+    img: team4,
     name: 'Mykhaylo Deriy',
     title: 'Operation Manager',
   },
   {
-    img: '../../src/assets/images/team5.jpg',
+    img: team5,
     name: 'Ksenia Nikishina',
     title: 'Office Manager',
   },
   {
-    img: '../../src/assets/images/team6.jpg',
+    img: team6,
     name: 'Anna Andriychuk',
     title: 'SMM-manager',
   },
   {
-    img: '../../src/assets/images/team7.jpg',
+    img: team7,
     name: 'Anastasia Kovalchuk',
     title: 'Director',
   },
   {
-    img: '../../src/assets/images/team8.png',
+    img: team8,
     name: 'Basil Gloo',
     title: 'Web Developer',
   },
   {
-    img: '../../src/assets/images/team9.jpg',
+    img: team9,
     name: 'Lina Yakobchuk',
     title: 'Designer',
   },
   {
-    img: '../../src/assets/images/team10.jpg',
+    img: team10,
     name: 'Anastasia Yevchenko',
     title: 'Translator',
   },
   {
-    img: '../../src/assets/images/hero.svg',
+    img: hero,
     name: 'Office Manager',
     title: 'open vacancy',
   },
   {
-    img: '../../src/assets/images/hero2.svg',
+    img: hero2,
     name: 'SEO specialist',
     title: 'open vacancy',
   },
